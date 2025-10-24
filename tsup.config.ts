c22f801b2834741dca7d2669a68cd1332c5be3bf
Danilo Mello <danilodemellow@gmail.com>
2025-10-24 00:56:28 -0300

    build: add ts build for bulding the ui package

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  outDir: 'dist',
+  minify: true,
+  sourcemap: true,
+  clean: true,
+  format: ['cjs', 'esm'],
+  external: ['react', 'react-dom'],
+  dts: true,
+  tsconfig: 'tsconfig.build.json',
+  esbuildOptions(options) {
+    options.conditions = ['style'];
+    return options;
+  },
+});
